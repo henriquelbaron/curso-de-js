6b3ae74c74d6b2b5958c799149eab476926845c8
henrique <hike.lemes@gmail.com>
2019-10-17 16:59:00 -0300

    Strings

---
+++ fundamentos/string.js
@@ -0,0 +1,19 @@
+const escola = "Senac"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(4));
+console.log(escola.indexOf('3'));
+
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+
+console.log('Escola '.concat(escola).concat("!"));
+console.log(escola.replace(/\w/g, 'e'));
+console.log(escola.replace(3, 'e'));
+
+console.log('Ana,Maria,Pedro'.split('.'));
+
+
+
+
